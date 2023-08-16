dc91edf1e50294234e9574d4d5d25ed61e50589a
Bautista Choren <bautistach999@hotmail.com>
2023-08-16 18:49:32 -0300

    quizYotros

--- logica.js
+++ logica.js
@@ -1,213 +1,204 @@
-const casas = [
-    {
-        casa:"Yronwood" ,
-        asentamiento:"Palosanto" ,
-        lema:"Nosotros protegemos el camino" ,
-        blason:"https://static.wikia.nocookie.net/hieloyfuego/images/3/35/Casa_Yronwood.png/revision/latest?cb=20130415204423" ,
-        blasonDescrpcion:"Un porticullis de sable en campo arena",
-        señor:"Lord Anders Yronwood" ,
-        region:"Dorne" ,
-        vasallaje:"casa Martell de Lanza del Sol" ,
-        guarnicion: {
-            caballeros:"22" ,
-            soldados:"1400" ,
-            arqueros:"500" ,
-        }
+/*Función constructora de las casas*/
 
-    },
-    {
-        casa:"Bolton" ,
-        asentamiento:"Fuerte Terror" ,
-        lema:"Nuestras espadas están afiladas" ,
-        blason:"https://static.wikia.nocookie.net/hieloyfuego/images/3/36/Casa_Bolton.png/revision/latest?cb=20161230200704" ,
-        blasonDescrpcion:"Un hombre desollado de gules en campo carnación de sangre",
-        señor:"Lord Roose Bolton" ,
-        region:"El Norte" ,
-        vasallaje:"Casa Stark de Invernalia" ,
-        guarnicion: {
+function Casa(casa, asentamiento, lema, blason, blasonDescrpcion, señor, region, vasallaje, guarnicion) {
+    this.casa = casa;
+    this.asentamiento = asentamiento;
+    this.lema = lema;
+    this.blason = blason;
+    this.blasonDescrpcion = blasonDescrpcion;
+    this.señor = señor;
+    this.region = region;
+    this.vasallaje = vasallaje;
+    this.guarnicion = guarnicion;
+}
+/*Array de las casas*/
+const casas = [
+    new Casa(
+        "Yronwood",
+        "Palosanto",
+        "Nosotros protegemos el camino",
+        "https://static.wikia.nocookie.net/hieloyfuego/images/3/35/Casa_Yronwood.png/revision/latest?cb=20130415204423",
+        "Un porticullis de sable en campo arena",
+        "Lord Anders Yronwood",
+        "Dorne",
+        "casa Martell de Lanza del Sol",
+        {
+            caballeros: "22",
+            soldados: "1400",
+            arqueros: "500",
+        }
+    ),
+    new Casa(
+        "Bolton" ,
+        "Fuerte Terror" ,
+        "Nuestras espadas están afiladas" ,
+        "https://static.wikia.nocookie.net/hieloyfuego/images/3/36/Casa_Bolton.png/revision/latest?cb=20161230200704" ,
+        "Un hombre desollado de gules en campo carnación de sangre",
+        "Lord Roose Bolton" ,
+        "El Norte" ,
+        "Casa Stark de Invernalia" ,
+         {
             caballeros:"0" ,
             soldados:"900" ,
             arqueros:"400" ,
-        }
-
-    },
-    {
-        casa:"Marbrand" ,
-        asentamiento:"Marcaceniza" ,
-        lema:"Ardiendo con intensidad" ,
-        blason:"https://static.wikia.nocookie.net/hieloyfuego/images/4/41/Casa_Marbrand.png/revision/latest?cb=20140602053653" ,
-        blasonDescrpcion:"Un árbol ardiendo, naranja sobre cenizo",
-        señor:"Lord Damon Marbrand" ,
-        region:"Tierras del Oeste" ,
-        vasallaje:"Casa Lannister de Roca Casterly" ,
-        guarnicion: {
+         }
+    ),
+    new Casa(
+        "Marbrand" ,
+        "Marcaceniza" ,
+        "Ardiendo con intensidad" ,
+        "https://static.wikia.nocookie.net/hieloyfuego/images/4/41/Casa_Marbrand.png/revision/latest?cb=20140602053653" ,
+        "Un árbol ardiendo, naranja sobre cenizo",
+        "Lord Damon Marbrand" ,
+        "Tierras del Oeste" ,
+        "Casa Lannister de Roca Casterly" ,
+        {
             caballeros:"16" ,
             soldados:"800" ,
             arqueros:"300" ,
         }
-
-
-    },
-    {
-        casa:"Mallister" ,
-        asentamiento:"Varamar" ,
-        lema:"Por encima del resto" ,
-        blason:"https://static.wikia.nocookie.net/hieloyfuego/images/1/11/Casa_Mallister.png/revision/latest?cb=20220720191300" ,
-        blasonDescrpcion:"Un águila plateada sobre campo púrpura",
-        señor:"Lord Jason Mallister" ,
-        region:"Tierra de los Ríos" ,
-        vasallaje:"Casa Tyrell de Altojardin" ,
-        guarnicion: {
+    ),
+    new Casa(
+        "Mallister" ,
+        "Varamar" ,
+        "Por encima del resto" ,
+        "https://static.wikia.nocookie.net/hieloyfuego/images/1/11/Casa_Mallister.png/revision/latest?cb=20220720191300" ,
+        "Un águila plateada sobre campo púrpura",
+        "Lord Jason Mallister" ,
+        "Tierra de los Ríos" ,
+        "Casa Tyrell de Altojardin" ,
+        {
             caballeros:"11" ,
             soldados:"660" ,
             arqueros:"190" ,
         }
-
-
-    },
-    {
-        casa:"Caron" ,
-        asentamiento:"Nocturnia" ,
-        lema:"Ninguna canción más dulce" ,
-        blason:"https://static.wikia.nocookie.net/hieloyfuego/images/1/1a/Casa_Caron.png/revision/latest?cb=20170529042907" ,
-        blasonDescrpcion:"Un campo de oro con ruiseñores de sable",
-        señor:"Lord Bryce Caron" ,
-        region:"Tierras de la Tormentas" ,
-        vasallaje:"Casa Baratheon de Bastión de Tormentas" ,
-        guarnicion: {
+    ),
+    new Casa(
+        "Caron" ,
+        "Nocturnia" ,
+        "Ninguna canción más dulce" ,
+        "https://static.wikia.nocookie.net/hieloyfuego/images/1/1a/Casa_Caron.png/revision/latest?cb=20170529042907" ,
+        "Un campo de oro con ruiseñores de sable",
+        "Lord Bryce Caron" ,
+        "Tierras de la Tormentas" ,
+        "Casa Baratheon de Bastión de Tormentas" ,
+        {
             caballeros:"7" ,
             soldados:"400" ,
             arqueros:"100" ,
         }
-
-
-    },
-    {
-        casa:"Royce" ,
-        asentamiento:"Piedra de las Runas" ,
-        lema:"Nosotros recordamos" ,
-        blason:"https://static.wikia.nocookie.net/hieloyfuego/images/5/5b/Casa_Royce.png/revision/latest?cb=20130502215119" ,
-        blasonDescrpcion:"Una lluvia de piedras sobre campo de bronce con bordura de runas",
-        señor:"Lord Yohn Royce" ,
-        region:"Valle de Arryn" ,
-        vasallaje:"Casa Arryn de Nido de Águilas" ,
-        guarnicion: {
+    ),
+    new Casa(
+        "Royce" ,
+        "Piedra de las Runas" ,
+        "Nosotros recordamos" ,
+        "https://static.wikia.nocookie.net/hieloyfuego/images/5/5b/Casa_Royce.png/revision/latest?cb=20130502215119" ,
+        "Una lluvia de piedras sobre campo de bronce con bordura de runas",
+        "Lord Yohn Royce" ,
+        "Valle de Arryn" ,
+        "Casa Arryn de Nido de Águilas" ,
+        {
             caballeros:"18" ,
             soldados:"1200" ,
             arqueros:"200" ,
         }
-
-
-    },
-    {
-        casa:"Oakheart" ,
-        asentamiento:"Roble viejo" ,
-        lema:"Nuestras Raíces son Profundas" ,
-        blason:"https://static.wikia.nocookie.net/hieloyfuego/images/d/d7/Casa_Oakheart.png/revision/latest?cb=20170529034614" ,
-        blasonDescrpcion:"Tres hojas de roble de sinople en campo de oro",
-        señor:"Lady Arwyn Oakheart" ,
-        region:"El Dominio" ,
-        vasallaje:"Casa Tyrell de Altojardin" ,
-        guarnicion: {
+    ),
+    new Casa(
+        "Oakheart" ,
+        "Roble viejo" ,
+        "Nuestras Raíces son Profundas" ,
+        "https://static.wikia.nocookie.net/hieloyfuego/images/d/d7/Casa_Oakheart.png/revision/latest?cb=20170529034614" ,
+        "Tres hojas de roble de sinople en campo de oro",
+        "Lady Arwyn Oakheart" ,
+        "El Dominio" ,
+        "Casa Tyrell de Altojardin" ,
+        {
             caballeros:"13" ,
             soldados:"330" ,
             arqueros:"100" ,
         }
-
-
-    },
-    {
-        casa:"Crakehall" ,
-        asentamiento:"Refugio Quebrado" ,
-        lema:"Ninguno más Fiero" ,
-        blason:"https://static.wikia.nocookie.net/hieloyfuego/images/a/a1/Casa_Crakehall.png/revision/latest?cb=20170514200929" ,
-        blasonDescrpcion:"Un jabalí negro y blanco sobre fondo marrón",
-        señor:"Lord Roland Crakehall" ,
-        region:"Tierras del Oeste" ,
-        vasallaje:"Casa Lannister de Roca Casterly" ,
-        guarnicion: {
+    ),
+    new Casa(
+        "Crakehall" ,
+        "Refugio Quebrado" ,
+        "Ninguno más Fiero" ,
+        "https://static.wikia.nocookie.net/hieloyfuego/images/a/a1/Casa_Crakehall.png/revision/latest?cb=20170514200929" ,
+        "Un jabalí negro y blanco sobre fondo marrón",
+        "Lord Roland Crakehall" ,
+        "Tierras del Oeste" ,
+        "Casa Lannister de Roca Casterly" ,
+        {
             caballeros:"8" ,
             soldados:"500" ,
             arqueros:"120" ,
         }
-
-
-    },
-    {
-        casa:"Hightower" ,
-        asentamiento:"Antigua" ,
-        lema:"Iluminamos el camino" ,
-        blason:"https://static.wikia.nocookie.net/hieloyfuego/images/a/a2/Casa_Hightower.png/revision/latest?cb=20130415202823" ,
-        blasonDescrpcion:"Un faro de piedra blanca con una llama en la punta",
-        señor:"Lord Leyton Hightower" ,
-        region:"El Dominio" ,
-        vasallaje:"Casa Tyrell de Altojardin" ,
-        guarnicion: {
+    ),
+    new Casa(
+        "Hightower" ,
+        "Antigua" ,
+        "Iluminamos el camino" ,
+        "https://static.wikia.nocookie.net/hieloyfuego/images/a/a2/Casa_Hightower.png/revision/latest?cb=20130415202823" ,
+        "Un faro de piedra blanca con una llama en la punta",
+        "Lord Leyton Hightower" ,
+        "El Dominio" ,
+        "Casa Tyrell de Altojardin" ,
+        {
             caballeros:"30" ,
             soldados:"1600" ,
             arqueros:"500" ,
         }
 
-
-    },
-    {
-        casa:"Fossoway" ,
-        asentamiento:"La Sidra" ,
-        lema:"El sabor de la gloria" ,
-        blason:"https://static.wikia.nocookie.net/hieloyfuego/images/9/9b/Casa_Fossoway_Manzana_Roja.png/revision/latest?cb=20140410190120" ,
-        blasonDescrpcion:"Una manzana de gules en campo de oro",
-        señor:"Lord Owen Fossoway" ,
-        region:"El Dominio" ,
-        vasallaje:"Casa Tyrell de Altojardin" ,
-        guarnicion: {
+    ),
+    new Casa(
+        "Fossoway" ,
+        "La Sidra" ,
+        "El sabor de la gloria" ,
+        "https://static.wikia.nocookie.net/hieloyfuego/images/9/9b/Casa_Fossoway_Manzana_Roja.png/revision/latest?cb=20140410190120" ,
+        "Una manzana de gules en campo de oro",
+        "Lord Owen Fossoway" ,
+        "El Dominio" ,
+        "Casa Tyrell de Altojardin" ,
+        {
             caballeros:"12" ,
             soldados:"700" ,
             arqueros:"510" ,
         }
 
-
-    },
-
-    {
-        casa:"Frey" ,
-        asentamiento:"Los Gemelos" ,
-        lema:"Desconocido" ,
-        blason:"https://static.wikia.nocookie.net/hieloyfuego/images/f/f0/Casa_Frey.png/revision/latest?cb=20161230201512" ,
-        blasonDescrpcion:"Dos torres gemelas azul oscuro sobre un fondo gris plateado",
-        señor:"Lord Walder frey" ,
-        region:"Tierra de los Rios" ,
-        vasallaje:"Casa Tully de Aguasdulces" ,
-        guarnicion: {
+    ),
+    new Casa(
+        "Frey" ,
+        "Los Gemelos" ,
+        "Desconocido" ,
+        "https://static.wikia.nocookie.net/hieloyfuego/images/f/f0/Casa_Frey.png/revision/latest?cb=20161230201512" ,
+        "Dos torres gemelas azul oscuro sobre un fondo gris plateado",
+        "Lord Walder frey" ,
+        "Tierra de los Rios" ,
+        "Casa Tully de Aguasdulces" ,
+        {
             caballeros:"25" ,
             soldados:"1500" ,
             arqueros:"440" ,
         }
 
-
-    },
-
-    {
-        casa:"Velaryon" ,
-        asentamiento:"Marcaceniza" ,
-        lema:"El Viejo, el Verdadero, el Valiente" ,
-        blason:"https://static.wikia.nocookie.net/hieloyfuego/images/f/f3/Casa_Velaryon.png/revision/latest?cb=20170514204249" ,
-        blasonDescrpcion:"Un caballo de mar de plata sobre campo verde mar",
-        señor:"Lord Monford Velaryon" ,
-        region:"Tierras de la Corona" ,
-        vasallaje:"Casa Baratheon de Rocadragón" ,
-        guarnicion: {
+    ),
+    new Casa(
+        "Velaryon" ,
+        "Marcaderiva" ,
+        "El Viejo, el Verdadero, el Valiente" ,
+        "https://static.wikia.nocookie.net/hieloyfuego/images/f/f3/Casa_Velaryon.png/revision/latest?cb=20170514204249" ,
+        "Un caballo de mar de plata sobre campo verde mar",
+        "Lord Monford Velaryon" ,
+        "Tierras de la Corona" ,
+        "Casa Baratheon de Rocadragón" ,
+        {
             caballeros:"9" ,
             soldados:"350" ,
             arqueros:"110" ,
         }
+    ),
+];
 
-
-    },
-    
-   
-
-
-]
+/*Función para unirse a una de las casas*/
 
 let usuarioUnido = false;
 
@@ -226,7 +217,7 @@ function unirseComoCaballero(casaNombre, casaBlason) {
 
         const mensaje = `Has puesto tu espada al servicio de ${casa.señor} de la Casa ${casa.casa} de ${casa.asentamiento}. Sirve con dedicación a tu nuevo señor.`;
         
-
+/*Cartel SweetAlert*/
         Swal.fire({
             title: '¡Te has unido como caballero!',
             icon: 'success',
@@ -242,14 +233,14 @@ function unirseComoCaballero(casaNombre, casaBlason) {
 
 
 
-
+/*Tarjetas de casas DOM*/
 
 function generarCard(casa) {
     return `
         <div class="col-md-3 mb-4 flex-fill">
             <div class="card h-100 bg-dark text-light">
                 <div class="card-header">
-                    <h5 class="card-title">${casa.casa}</h5>
+                    <h5 class="card-title">Casa  ${casa.casa}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">${casa.asentamiento}</h6>
                 </div>
                 <img src="${casa.blason}" class="card-img-top" alt="Blason de ${casa.casa}">
---
+++ pages/quiz.js
@@ -0,0 +1,91 @@
+const quizContainer = document.getElementById('quiz-container');
+const submitButton = document.getElementById('submit-btn');
+const timerContainer = document.getElementById('timer'); // Nuevo elemento para mostrar el temporizador
+
+let score = 0;
+let questions = [];
+
+let timeElapsed = 0; // Tiempo en segundos transcurridos
+let timerInterval; // Variable para almacenar el intervalo del temporizador
+
+// Función para iniciar el temporizador
+function startTimer() {
+    timerInterval = setInterval(updateTimer, 1000); // Actualizar cada segundo
+}
+
+// Función para actualizar el temporizador
+function updateTimer() {
+    timeElapsed++;
+    document.getElementById('time').textContent = timeElapsed;
+}
+
+// Obtener preguntas usando Fetch API
+fetch('preguntas.json')
+    .then(response => response.json())
+    .then(data => {
+        questions = data; 
+        createQuiz(); // Crear el quiz al cargar las preguntas
+        startTimer(); // Iniciar el temporizador al cargar las preguntas
+    });
+
+// Función para crear el quiz
+function createQuiz() {
+    questions.forEach((question, index) => {
+        const questionDiv = document.createElement('div');
+        questionDiv.className = 'card mt-4';
+        questionDiv.innerHTML = `
+            <div class="card-body">
+                <h5 class="card-title">Pregunta ${index + 1}:</h5>
+                <p class="card-text">${question.pregunta}</p>
+                ${question.opciones.map((opcion, optionIndex) => `
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="question-${index}" id="option-${index}-${optionIndex}" value="${optionIndex}">
+                        <label class="form-check-label" for="option-${index}-${optionIndex}">
+                            ${opcion}
+                        </label>
+                    </div>
+                `).join('')}
+            </div>
+        `;
+        quizContainer.appendChild(questionDiv);
+    });
+}
+
+// Calcular resultado al hacer clic en "Enviar Respuestas"
+submitButton.addEventListener('click', () => {
+    clearInterval(timerInterval); // Detener el temporizador al enviar respuestas
+    calculateScore();
+    showResults();
+});
+
+// Función para calcular el puntaje
+function calculateScore() {
+    const questionDivs = document.querySelectorAll('.card');
+    
+    questionDivs.forEach((questionDiv, index) => {
+        const selectedOption = questionDiv.querySelector(`input[name="question-${index}"]:checked`);
+        if (selectedOption) {
+            const selectedValue = parseInt(selectedOption.value);
+            if (selectedValue === questions[index].respuesta) {
+                score++;
+            }
+        }
+    });
+}
+
+// Función para mostrar los resultados y el botón "Try Again"
+function showResults() {
+    Swal.fire({
+        title: 'Resultados del Quiz',
+        html: `Tu puntaje es: ${score} de ${questions.length} preguntas<br>¡Inténtalo de nuevo si lo deseas!`,
+        icon: 'info',
+        showCancelButton: true,
+        confirmButtonText: 'Try Again',
+        cancelButtonText: 'Cerrar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            location.reload(); // Reiniciar la página para volver a intentar
+        }
+    });
+}
+
